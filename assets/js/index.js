49095a62acb37ea1d61609de00ef9b9e573efb84
camila santibañez <cspavez5@gmail.com>
2024-03-01 18:11:02 -0300

    info

---
+++ assets/js/index.js
@@ -0,0 +1,16 @@
+precio  = 400000;
+let cantidadInicial= 1;
+
+let precioSpan = document.querySelector (".precio-inicial");
+let cantidad = document.querySelector (".cantidad");
+let total = document.querySelector (".valor-total")
+
+precioSpan.innerHTML = precio;
+cantidad.innerHTML = cantidadInicial;
+total.innerHTML = precio;
+
+
+
+
+
+
